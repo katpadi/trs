567488204390a1993fee7dfb508466fcbebfa806
Kat Padilla <hello@katpadi.ph>
2017-11-26 17:57:00 +0800

    Add CLI and command center

---
+++ lib/command_center.rb
@@ -0,0 +1,20 @@
+class CommandCenter
+  VALID = %w(left right move place report).freeze
+
+  attr_reader :robot, :command_input
+  def initialize(robot)
+    @robot = robot
+  end
+
+  def analyze(command_input = nil)
+    return if command_input.nil?
+    command_input, args = command_input.chomp.split(' ')
+    command_input = "#{command_input}".downcase
+
+    unless VALID.include?(command_input)
+      return '[ERROR]: Invalid command'.red
+    end
+
+    @robot.send(:"#{command_input}", args)
+  end
+end
---
+++ lib/trs/cli.rb
@@ -0,0 +1,59 @@
+require "thor"
+require "colorize"
+require "trs"
+
+module Trs
+  class CLI < Thor
+    desc 'output [PATH TO FILE]', 'print out the contents of file'
+    method_option :length, type: :numeric, default: 5, aliases: "-l"
+    method_option :width,  type: :numeric, default: 5, aliases: "-w"
+    def output(file_name)
+      contents = File.read(file_name)
+      line_num = 0
+      contents.gsub!(/\r\n?/, "\n")
+
+      table = Table.new(options[:length], options[:width])
+      robot = Robot.new(table)
+      command_ctr = CommandCenter.new(robot)
+
+      contents.each_line do |text|
+        puts "COMMAND ##{line_num += 1}: #{text}"
+        puts command_ctr.analyze(text)
+      end
+    end
+
+    desc 'play', 'Starts the Toy Robot Simulator app'
+    method_option :length, type: :numeric, default: 5, aliases: "-l"
+    method_option :width,  type: :numeric, default: 5, aliases: "-w"
+    def play
+      say welcome_greeting, :yellow
+      table = Table.new(options[:length], options[:width])
+      robot = Robot.new(table)
+      command_ctr = CommandCenter.new(robot)
+      loop do
+        say 'Input command (BYE to quit): '
+        input = STDIN.gets
+        if input =~ /bye/i
+          break
+        else
+          puts command_ctr.analyze(input)
+        end
+      end
+    end
+
+    no_commands do
+      def welcome_greeting
+        <<-long_desc
+          WELCOME TO TOY ROBOT SIMULATION!
+
+          Valid Commands:
+          PLACE  -  Specifies position of robot in X,Y,F format.
+          MOVE   -  Moves robot one unit forward in the direction it is currently facing
+          LEFT   -  Rotates robot 90 degrees to the left
+          RIGHT  -  Rotates robot 90 degrees to the right
+          REPORT -  Shouts the current position
+        long_desc
+      end
+    end
+  end
+end
